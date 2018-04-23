ec15b67afbf0af8ef63d13649c166ee870a207ba
madpome <earphilippefr@gmail.com>
2018-04-23 17:48:41 +0200

    début decode

---
+++ Huffman_Dynamic/decode_huff_dyn.c
@@ -0,0 +1,89 @@
+#include  "arbre.h"
+
+int read_header(int desc){
+    char *s = calloc(5,sizeof(char));
+    read(desc, s, 4*sizeof(char));
+    s[4]='\0';
+    return (strcmp(s,"HDMI")!=0)?-1:1;
+}
+
+int readLetter(int descR, int descW, noeud *arbre, int * nbNode){
+    char c;
+    //Lecture de 2 fois
+    int r = read(descR, &c, 1);
+    if(r <= 0){
+        //Ca ne devrait pas arriver, il y a une erreur
+        fprintf(stderr,"Erreur lecture\n");
+        exit(-1);
+    }
+    if(c != 0){
+        //Fin du fichier
+        return -1;
+    }
+    r = read(descR, &c, 1);
+    if(r <= 0){
+        //Ca ne devrait pas arriver, il y a une erreur
+        fprintf(stderr,"Erreur lecture2\n");
+        exit(-1);
+    }
+    //On l'ajoute à l'arbre
+    addCharInTree(arbre, c, nbNode);
+    //Et on l'écrit bien sur
+    if(write(descR, &c, 1) < 0){
+        perror("");
+        exit(-1);
+    }
+    return 1;
+}
+
+
+int main(int argc, char **argv){
+    if(argc != 3){
+        fprintf(stderr,"2 arguments : entrée et sortie\n");
+        return -1;
+    }
+    int descRead = open(argv[1], O_RDONLY);
+    int descWrite = open(argv[2], O_WRONLY | O_CREAT);
+    if(descRead <0){
+        fprintf(stderr, "Impossible d'ouvrir en lecture %s\n",argv[1]);
+    }
+    if(descWrite < 0){
+        fprintf(stderr, "Impossible d'ouvrir en écriture %s\n",argv[2]);
+
+    }
+    if(read_header(descRead)!=1){
+        fprintf(stderr,"Header incorrect\n");
+        return -1;
+    }
+    noeud *arbre = setArbre();
+    int nbNode = 1;
+    readLetter(descRead, descWrite, arbre, &nbNode);
+
+    unsigned char c;
+    int idx =0;
+    unsigned char bitTab[8];
+    for(int i = 0;i<8;i++){
+        bitTab[i] = 0 | (1<<i);
+    }
+    int pos = nbNode-1;
+    noeud cur = arbre[pos];
+    while(1){
+        read(descRead, &c, 1);
+        for(idx = 0;idx<8 ;idx++){
+            //Si le bit a la pos idx est en 1
+            if((c & bitTab[idx])== bitTab[idx]){
+                pos += cur.dfg;
+            }else{
+                pos += cur.dfd;
+            }
+            cur = arbre[pos];
+            //On est a une lettre
+            if(cur.dfg == 0 && cur.dfd==0){
+                write(descWrite, &cur.lettre, 1);
+                pos = nbNode-1;
+                cur=arbre[pos];
+            }
+        }
+
+    }
+}
