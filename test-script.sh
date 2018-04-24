8d76450fb712b0a94c56f2b86fbcc6a3209cc3de
madpome <earphilippefr@gmail.com>
2018-04-24 22:42:06 +0200

    hello

--- test-script.sh
+++ test-script.sh
@@ -1,24 +1,82 @@
 #!/bin/bash
-TEST_FILE="Explod.png MobyDick.txt AmerikastaPalatessa.txt Chinois.txt \
-Florante.txt image1.jpeg image2.jpeg image3.png image4.png \
-OurFriendtheDog.txt"
+TEST_IMG="Explod.png image1.jpeg image2.jpeg image3.png image4.png"
+TEST_TXT="OurFriendtheDog.txt MobyDick.txt AmerikastaPalatessa.txt Chinois.txt Florante.txt"
+TEST_AUDIO=
 CD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
-WD=$CD"test_huff_stat/"
-HUFF=$CD"huffman"
-DECOD=$HUFF"Decod"
-echo "Testing script for static huffman compressor"
-
-for file in $TEST_FILE
+echo "$CD"
+STATIC=$CD"Huffman_Static/"
+ADAPTATIVE=$CD"Huffman_Dynamic/"
+WD=$CD"test_compression/"
+IMG=$WD"img/"
+AUDIO=$WD"audio/"
+TXT=$WD"txt/"
+ENCOD=$STATIC"encodeHuff"
+DECOD=$STATIC"decodeHuff"
+echo "$ENCOD"
+echo "$DECOD"
+echo "----TESTING SCRIPT STATIC HUFFMAN----"
+echo "****TESTING TEXT***"
+for file in $TEST_TXT
 do
     echo ""
-    echo "Compressing $file to $file.comp"
-    eval "$HUFF $WD$file $WD$file.comp"
-    echo "Decompressing $file.comp to $file.huffed"
-    eval "$DECOD $WD$file.comp $WD$file.huffed"
-    echo "Removing $WD$file.comp"
-    eval "rm -f $WD$file.comp"
-    echo "Diff between $file and $file.huffed"
-    eval "diff $WD$file $WD$file.huffed"
+    echo "Compressing $file to $file.compStat"
+    eval "$ENCOD $TXT$file $TXT$file.comStat"
+    echo "Decompressing $file.comStat to $file.decompStat"
+    eval "$DECOD $TXT$file.comStat $TXT$file.decompStat"
+    echo "Removing $TXT$file.comStat"
+    eval "rm -f $TXT$file.comStat"
+    echo "Diff between $file and $file.decompStat"
+    eval "diff $TXT$file $TXT$file.decompStat"
     echo "End diff"
 done
+
+echo "****TESTING IMAGE***"
+for file in $TEST_IMG
+do
+    echo ""
+    echo "Compressing $file to $file.comStat"
+    eval "$ENCOD $IMG$file $IMG$file.comStat"
+    echo "Decompressing $file.comStat to $file.decompStat"
+    eval "$DECOD $IMG$file.comStat $IMG$file.decompStat"
+    echo "Removing $IMG$file.comStat"
+    eval "rm -f $IMG$file.comStat"
+    echo "Diff between $file and $file.decompStat"
+    eval "diff $IMG$file $IMG$file.decompStat"
+    echo "End diff"
+done
+
+ENCOD=$ADAPTATIVE"encodeHuff"
+DECOD=$ADAPTATIVE"decodeHuff"
+echo ""
+echo "----TESTING SCRIPT ADAPTATIVE HUFFMAN----"
+echo "****TESTING TEXT***"
+for file in $TEST_TXT
+do
+    echo ""
+    echo "Compressing $file to $file.comDyn"
+    eval "$ENCOD $TXT$file $TXT$file.comDyn"
+    echo "Decompressing $file.comDyn to $file.decompDyn"
+    eval "$DECOD $TXT$file.comDyn $TXT$file.decompDyn"
+    echo "Removing $TXT$file.comDyn"
+    eval "rm -f $TXT$file.comDyn"
+    echo "Diff between $file and $file.decompDyn"
+    eval "diff $TXT$file $TXT$file.decompDyn"
+    echo "End diff"
+done
+
+echo "****TESTING IMAGE***"
+for file in $TEST_IMG
+do
+    echo ""
+    echo "Compressing $file to $file.comDyn"
+    eval "$ENCOD $IMG$file $IMG$file.comDyn"
+    echo "Decompressing $file.comDyn to $file.decompDyn"
+    eval "$DECOD $IMG$file.comDyn $IMG$file.decompDyn"
+    echo "Removing $IMG$file.comDyn"
+    eval "rm -f $IMG$file.comDyn"
+    echo "Diff between $file and $file.decompDyn"
+    eval "diff $IMG$file $IMG$file.decompDyn"
+    echo "End diff"
+done
+
 exit 0
